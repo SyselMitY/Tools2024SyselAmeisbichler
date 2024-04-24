c51e1b5a989c507c78ffa785e3a89e715cd3a753
Emma Sysel <peter@sysel.at>
2024-04-24 16:01:19 +0200

    meine neue funktion :)

--- sysel_ameisbichler_android/src/coolespackage/coolesprogramm.java
+++ sysel_ameisbichler_android/src/coolespackage/coolesprogramm.java
@@ -3,3 +3,7 @@ package coolespackage;
 public static void main(String[] args) {
 	System.out.prinln("Servas Wöd");
 }
+
+public void cooleSyselMethode() {
+	System.out.println("Angabeleser wissen mehr");
+}
