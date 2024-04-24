95bec98d83d9c9620a28a53f8b26e45bc73ad707
krane <christian.kranabetter@protonmail.com>
2024-04-24 18:12:06 +0200

    introduce bugs >:)

--- sysel_ameisbichler_android/src/coolespackage/coolesprogramm.java
+++ sysel_ameisbichler_android/src/coolespackage/coolesprogramm.java
@@ -1,16 +1,13 @@
-package coolespackage;
+package uncoolespackage; // HAHAHA habe einen ERROR eingebaut >:D
 
-public static void main(String[] args) {
-	System.out.prinln("Servas Wöd, Tschüss Thomas");
+public static void main(String[]args){System.out.prinln("Servas Wöd, Tschüss Thomas");}
+
+public void thisIsAnOldMethod(){int counter;
+// this.thisIsANewMethod(); //Does nothing.
+counter++; // Syntax Error >:(
 }
 
-public void thisIsANewMethod() {
-	int counter;
-	//this.thisIsANewMethod(); //Does nothing.
-	counter++;
-}
-
-public void cooleSyselMethode() {
-	//ACHTUNG, Angabeleser wissen nicht immer mehr, Angabelesen erhöht nur WIS, nicht INT
-	System.out.println("Angabeleser wissen mehr");
-}
+public void uncooleSyselMethode(){
+// ACHTUNG, Angabeleser wissen nicht immer mehr, Angabelesen erhöht nur WIS,
+// nicht INT
+System.out.println("Angabeleser wissen weniger! HIHIHI HA WERDE GEHACKT >:)");}
