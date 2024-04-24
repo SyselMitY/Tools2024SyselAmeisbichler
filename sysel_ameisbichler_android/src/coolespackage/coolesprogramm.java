4b4398dbf391de69eb3dc86c1abf2f7d295eefc7
Mia <mia@localhost.localdomain>
2024-04-24 15:52:45 +0200

    Added new Method, Changed Hello World

--- sysel_ameisbichler_android/src/coolespackage/coolesprogramm.java
+++ sysel_ameisbichler_android/src/coolespackage/coolesprogramm.java
@@ -1,5 +1,9 @@
 package coolespackage;
 
 public static void main(String[] args) {
-	System.out.prinln("Servas Wöd");
+	System.out.prinln("Servas Wöd, Tschüss Wöd");
+}
+
+public void thisIsANewMethod() {
+
 }
